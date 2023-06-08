3b1d72c9351b801fab7ea9f7144859cca952c677
suthyscott <sutherlandscott21@gmail.com>
2023-06-08 15:41:40 -0600

    cards are draggable

--- src/elements/BookCard.jsx
+++ src/elements/BookCard.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react"
+import { useNavigate } from "react-router-dom"
+import Draggable from "react-draggable"
 
-const BookCard = ({book}) => {
-  const navigate = useNavigate()
-  return (
-    <div className='h-[300px] w-[300px] border-solid border border-color-primary rounded-2xl flex flex-col justify-center items-center'>
-      <h2>{book.title}</h2>
-      <p>{book.author}</p>
-      <p>{book.length} pages</p>
-      <p>{book.desc}</p>
-      <button onClick={() => navigate(`/details/${book.id}`)}>See more book details</button>
-    </div>
-  )
+const BookCard = ({ book }) => {
+    const navigate = useNavigate()
+
+    const eventHandler = (e, data) => {
+      console.log(e, data)
+    }
+
+    return (
+        <Draggable
+          onStop={eventHandler}
+        >
+            <div className="h-[300px] w-[300px] border-solid border border-color-primary rounded-2xl flex flex-col justify-center items-center">
+                <h2>{book.title}</h2>
+                <p>{book.author}</p>
+                <p>{book.length} pages</p>
+                <p>{book.desc}</p>
+                <button onClick={() => navigate(`/details/${book.id}`)}>
+                    See more book details
+                </button>
+            </div>
+        </Draggable>
+    )
 }
 
 export default BookCard
